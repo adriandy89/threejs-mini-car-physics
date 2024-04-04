3625640cbfd558fc5ead87c63a8bcb1763be8981
Adrian DY <adrian.dy89@gmail.com>
2024-04-04 15:00:15 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import { Scene } from "./Scene";
+import { Physics } from "@react-three/cannon";
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <Canvas>
+      <Physics
+        broadphase="SAP"
+        gravity={[0, -4, 0]}
+      >
+        <Scene />
+      </Physics>
+    </Canvas>
+
+    <div class="controls">
+      <p><b>R</b>: reset</p>
+      <p><b>W A S D</b>: move</p>
+      <p><b>C</b>: swap camera</p>
+      <p><b>arrows</b> for flips</p>
+    </div>
+  </>
+);
